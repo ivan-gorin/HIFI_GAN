ac7c6b4699b391d674c200ee7412062875d56837
ivan <ivangorin21@yandex.ru>
2021-12-19 16:38:33 +0300

    fixes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 mkdir data
-cd data
+cd data || exit
 if ! [[ -d "LJSpeech-1.1" ]]; then
     if ! [[ -f "LJSpeech-1.1.tar.bz2" ]]; then
         wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
